d06861ed039b7e018b7fa9ee817741c2586cb944
Usong Cho <silvermouse4012@gmail.com>
2018-10-03 19:08:24 -0700

    added functionality

--- todos.js
+++ todos.js
@@ -1,12 +1,35 @@
 let todoField = document.querySelector('.todoField');
 let addTodo = document.querySelector('.addTodo');
 let todoList = document.querySelector('.todoList');
+let searchField = document.querySelector('.searchField');
 
-let todos =[];
+let todos =[{
+    name: 'todo1',
+    isDone: false,
+    show: true,
+}, {
+    name: 'todo2',
+    isDone: true,
+    show: true,
+}];
 
 addTodo.addEventListener('click', addTodos);
 todoField.addEventListener('keypress', addOnEnter);
+searchField.addEventListener('input', search);
 todoField.focus();
+renderTodos();
+
+function search(e) {
+    let searchValue = e.target.value;
+    todos.forEach((todo, index) => {
+        if (todo.name.indexOf(searchValue) !== -1) {
+            todo.show = true;
+        } else {
+            todo.show = false;
+        };
+    });
+    renderTodos();
+};
 
 function addOnEnter(e) {
     let keyCode = e.keyCode;
@@ -14,10 +37,7 @@ function addOnEnter(e) {
         todoField.focus();
         return null;
     } else if (keyCode === 13) {
-        todoField.focus();
-        todos.push(todoField.value);
-        todoField.value = '';
-        renderTodos();
+        addTodos();
     };
 };
 
@@ -27,7 +47,11 @@ function addTodos() {
         return null;
     }
     todoField.focus();
-    todos.push(todoField.value);
+    todos.push({
+        name: todoField.value,
+        isDone: false,
+        show: true,
+    });
     todoField.value = '';
     renderTodos();
 };
@@ -51,14 +75,20 @@ function addRemoveButtonEventListener() {
             renderTodos();
         });
     });
+    let checkBoxes = document.querySelectorAll('.todoList input[type="checkbox"]');
+    checkBoxes.forEach((checkBox, index) => {
+        checkBox.addEventListener('change', (e) => {
+            todos[index].isDone = e.target.checked;
+        });
+    });
 };
 
-function getTodoHtml(text, index) {
+function getTodoHtml(todoObj, index) {
     let Html = `
-        <li>
-            <span>${text}</span> 
+        <li style="display:${todoObj.show ? 'block' : 'none'}">
+            <span>${todoObj.name}</span> 
             <div class="controls">
-            <input type="checkbox" />
+            <input type="checkbox" class="checkbox_${index}" ${todoObj.isDone ? 'checked' : ''} />
                 <button class="button_${index}">
                     remove
                 </button>
